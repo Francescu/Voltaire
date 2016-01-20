85a1e816fd601fb75dacce43a143c6efd4e32d5e
Francescu Santoni <francescu@stupeflix.com>
2016-01-20 22:08:23 +0100

    Updated server to print swift image

--- server/main.swift
+++ server/main.swift
@@ -1,14 +1,20 @@
+#if os(Linux)
+import Glibc
+#endif
+
 import Foundation
 import Swifter
 
 let server = HttpServer()
 
 server["/public/:path"] = HttpHandlers.directory("../client/public/")
-server["/hello"] = { .OK(HttpResponseBody.Html("You asked for " + $0.path)) }
+server["/"] = { request in
+  return .OK(HttpResponseBody.Html("<img src='./public/swift.png' />")) 
+}
 
 try! server.start()
 
-    while true {
-        sleep(1)
-    }
+while true {
+    sleep(1)
+}
 
